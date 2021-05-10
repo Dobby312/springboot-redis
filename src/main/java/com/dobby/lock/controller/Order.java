66184ad38497558263a74e6a8d5737ee4224038b
wenzufan <2287593100@qq.com>
2021-05-10 22:12:05 +0800

    redis-cell实现接口限流

--- src/main/java/com/dobby/lock/controller/Order.java
+++ src/main/java/com/dobby/lock/controller/Order.java
@@ -5,12 +5,15 @@
 package com.dobby.lock.controller;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +32,8 @@ import lombok.extern.slf4j.Slf4j;
 public class Order {
 	@Autowired
 	private JedisCom jedisCom;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
 
 	private long nKuCun = 0;
 
@@ -110,5 +115,36 @@ public class Order {
             }
         }
         return "";
+    }
+
+    /**
+     * API调用频率
+     * @param apiName 接口名称
+     * @param rate 频率/秒
+     * @return
+     */
+    public boolean apiRate(String apiName, int rate) {
+        try {
+            /**
+             * redis-cli 命令
+             eval "return redis.call('cl.throttle',KEYS[1], ARGV[1], ARGV[2], ARGV[3], ARGV[4])[1];" 1 key 3 3 10 1
+             */
+            DefaultRedisScript script = new DefaultRedisScript();
+            script.setResultType(Long.class);
+            /*
+                cl.throttle命令返回的是一个table，这里只关注第一个元素0表示正常，1表示过载
+                KEYS[1]需要设置的key值，结合业务需要可以是接口名称+用户ID+购买的资源包等等等等
+                ARGV[1]参数是漏斗肚子的大小  个人理解加上实测：此值越小越接近设置的rate,越大越有令牌桶的味道会有瞬时超过rate
+                ARGV[2]频率次数，结合ARGV[3]一起使用
+                ARGV[3]周期（秒），结合ARGV[2]一起使用
+                ARGV[4] 不知道是什么没用上
+             */
+            script.setScriptText("return redis.call('cl.throttle',KEYS[1], ARGV[1], ARGV[2], ARGV[3], ARGV[4])[1]");
+            Long rst = (Long)redisTemplate.execute(script, Arrays.asList(apiName), 1,rate,1,1);
+            return rst == 0 ;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 }
